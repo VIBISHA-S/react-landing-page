22affcf7c3ecde5c07c890f166436fd376922e30
VIBISHA-S <vibisha.edu@gamil.com>
2025-04-25 23:26:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <h1>Welcome to My Landing Page</h1>
+        <p>Discover our amazing product features and services.</p>
+        <button>Get Started</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
