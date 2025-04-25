5fa43711a99fa841d263108ec95eb0f40089cad0
VIBISHA-S <vibisha.edu@gamil.com>
2025-04-26 00:21:18 +0530

    Upated heading text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="container">
       <header>
-        <h1>Welcome to My Landing Page</h1>
+        <h1>Updated Landing Page - Hello Vercel!</h1>
         <p>Discover our amazing product features and services.</p>
         <button>Get Started</button>
       </header>
